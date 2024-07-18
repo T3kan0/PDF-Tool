3ab202d99dc949bd0f0ca5f01cad72e546bc3a56
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2024-07-18 15:29:48 +0200

    Update oracle_app.py

--- oracle_app.py
+++ oracle_app.py
@@ -55,7 +55,7 @@ if len(uploaded_file) >= 0:
         captions = ["Default: Stacking Files Atop One Another 📕📗📘➡️📚",
                 "Separates Files: From One Document to Many 📚➡️📕📗📘"]
         )
-        st.write(' ')
+        #st.write(' ')
         st.markdown('---')
 
 # Set a fixed height for the iframes
